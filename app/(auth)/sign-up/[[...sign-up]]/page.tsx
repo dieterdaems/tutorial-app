3eadd57fc562f301859ab2496d9dac511da93084
dieterdaems <dieter.daems@student.ucll.be>
2024-07-03 18:11:51 +0200

    log in and log out with styling

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,31 @@
-const SignUpPage = () => {
-    return (
-        <div>
-        <h1>Sign Up</h1>
-        </div>
-    );
-    };
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
 
-export default SignUpPage;
+export default function Page() {
+    return (
+      <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+        <div className="h-full lg:flex flex-col items-center justify-center px-4">
+            <div className="text-center space-y-4 pt-16">
+                <h1 className="font-bold text-3xl text-[#2E2A47]">
+                    Welcome Back!
+                </h1>
+                <p className="text-base text-[#7E8CA0]">
+                    Log in or create account to continue
+                </p>
+            </div>
+            <div className="flex items-center justify-center mt-8">
+                <ClerkLoaded>
+                    <SignUp />;
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className="animate-spin text-muted-foreground" />
+                </ClerkLoading>
+            </div>
+        </div>
+        <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+            <Image src={"/vercel.svg"} width={100} height={100} alt={""}  />
+        </div>
+      </div>
+    )
+}
