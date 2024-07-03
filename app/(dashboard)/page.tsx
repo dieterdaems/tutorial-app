c3d58068a358b72fdc8d325a70bafc3cc1786aaa
dieterdaems <dieter.daems@student.ucll.be>
2024-07-03 18:12:18 +0200

    quick logout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
