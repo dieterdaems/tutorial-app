4aee9c749d26a9b1f09e7ebf1db956253876661f
dieterdaems <dieter.daems@student.ucll.be>
2024-07-05 01:59:33 +0200

    added get route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,43 +1,24 @@
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import accounts from './accounts'
+import { HTTPException } from 'hono/http-exception'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app 
-    .get('/hello', 
-    clerkMiddleware(),
-    (c) => {
-        const auth = getAuth(c)
-        if (!auth?.userId) {
-            return c.json({ error: 'unauthorized' })
-        }
-    return c.json({
-         message: 'Hello Next.js!',
-         userId: auth.userId,
-     })
-    })
-    .get(
-        '/hello/:name',
-        zValidator("param", z.object({ name: z.string() })  ),
-        (c) => {
-            const  { name }   = c.req.valid("param")
-            return c.json({
-                message: `Hello ${name}!`,
-                name: name,
-                })
-        })
-    .post(
-        '/',
-        zValidator("json", z.object({ name: z.string(), userId: z.string(), })  ),
-        (c) => {
-            const {name, userId} = c.req.valid("json")
-            return c.json({})
-        })
+app.onError((error, c) => {
+    if (error instanceof HTTPException) {
+        return error.getResponse()
+    }
+    return c.json({ error: "Internal error" }, 500)
+}
+)
+
+const routes = app
+    .route('accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes
