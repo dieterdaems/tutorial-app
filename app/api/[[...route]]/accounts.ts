a589df5a81fef83ae6c8048f76014f1d187d46e4
dieterdaems <dieter.daems@student.ucll.be>
2024-07-11 21:12:42 +0200

    get account by id, api + hook

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -5,7 +5,7 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
 import { eq, and, inArray } from "drizzle-orm";
 import { zValidator} from '@hono/zod-validator';
 import { createId} from '@paralleldrive/cuid2';
-import { z } from "zod";
+import { optional, z } from "zod";
 
 const app = new Hono()
     .get("/",
@@ -21,6 +21,39 @@ const app = new Hono()
         .from(accounts)
         .where(eq(accounts.userId, auth.userId))
         return c.json({ data });
+        })
+    .get(
+        "/:id",
+        clerkMiddleware(),
+        zValidator("param", z.object({ id: z.string().optional() })),
+        async (c) => {
+            const auth = getAuth(c);
+            const { id } = c.req.valid("param");
+
+            if (!id) {
+                return c.json({ error: "Invalid id" }, 400);
+            }
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const [data] = await db
+                .select({ id: accounts.id, name: accounts.name })
+                .from(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        eq(accounts.id, id)
+                    )
+                );
+
+            if (!data) {
+                return c.json({ error: "Not found" }, 404);
+            }
+
+            return c.json({ data });
+        
         })
     .post(
         "/",
@@ -70,8 +103,6 @@ const app = new Hono()
             });
 
             return c.json({ data });
-        }
-        
-    );
+        });
 
 export default app;
