c905242a718dd7575d0df05279e16cfae8436c55
dieterdaems <113980664+dieterdaems@users.noreply.github.com>
2024-08-20 11:17:56 +0200

    category DB and routes added

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
+import categories from './categories'
 import { HTTPException } from 'hono/http-exception'
 
 export const runtime = 'edge'
@@ -8,7 +9,8 @@ export const runtime = 'edge'
 const app = new Hono().basePath('/api')
 
 const routes = app
-    .route('accounts', accounts)
+    .route('/accounts', accounts)
+    .route('/categories', categories)
 
 export const GET = handle(app)
 export const POST = handle(app)
