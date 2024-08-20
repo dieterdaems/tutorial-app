e741fb787e3d1f0006591c4560e7bcc3081f0892
dieterdaems <113980664+dieterdaems@users.noreply.github.com>
2024-08-20 11:18:30 +0200

    category components

---
+++ features/categories/components/edit-category-sheet.tsx
@@ -0,0 +1,79 @@
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { CategoryForm } from "./category-form";
+import { insertCategorySchema } from "@/db/schema"
+import { z } from "zod"
+import { useOpenCategory } from "../hooks/use-open-category";
+import { useGetCategory } from "../api/use-get-category";
+import { useEditCategory } from "../api/use-edit-category";
+import { Loader2 } from "lucide-react";
+import { useDeleteCategory } from "../api/use-delete-category";
+import { useConfirm } from "@/hooks/use-confirm";
+
+
+
+const formSchema = insertCategorySchema.pick({
+    name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const EditCategorySheet = () => {
+    const { isOpen, onClose, id } = useOpenCategory();
+
+    const [ConfirmDialog, confirm] = useConfirm(
+        'Are you sure?',
+        'This action cannot be undone',
+    )
+
+    const categoryQuery = useGetCategory(id);
+    const editMutation = useEditCategory(id);
+    const deleteMutation = useDeleteCategory(id);
+
+    const isLoading = categoryQuery.isLoading;
+    const isPending = editMutation.isPending || deleteMutation.isPending;
+    
+
+    const onSubmit = (values: FormValues) => {
+        editMutation.mutate(values, { onSuccess: () => onClose() });
+    };
+
+    const onDelete = async () => {
+        const confirmed = await confirm();
+
+        if (confirmed) {
+            deleteMutation.mutate(undefined, {
+                onSuccess: () => onClose()
+            });
+        }
+
+    }
+
+    const defaultValues = categoryQuery.data ? { name: categoryQuery.data.data.name } : { name: '' };
+    return (
+        <>  
+            <ConfirmDialog />
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent className="space-y-4">
+                    <SheetHeader>
+                    <SheetTitle>Edit Category</SheetTitle>
+                    <SheetDescription>
+                    Edit an existing category
+                    </SheetDescription>
+                    </SheetHeader>
+                    {isLoading ? 
+                    (<div className="absolute inset-0 flex items-center justify-center">
+                        <Loader2 className="size-4 animate-spin text-muted-foreground"/>
+                    </div>)
+                    :(
+                        <CategoryForm
+                        id={id} 
+                        onSubmit={onSubmit} 
+                        disabled={isPending} 
+                        defaultValues={defaultValues} 
+                        onDelete={onDelete} />
+                    )}
+                </SheetContent>
+            </Sheet>
+        </>
+    );
+    };
