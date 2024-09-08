f73fedd836be1bb38074f9e8284dadedc3af47ca
dieterdaems <113980664+dieterdaems@users.noreply.github.com>
2024-09-08 15:42:58 +0200

    added calculatepercentage and fillmissingdays to util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { eachDayOfInterval, isSameDay } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -19,4 +20,45 @@ export function formatCurrency(amount: number) {
     currency: 'EUR',
     minimumFractionDigits: 2,
   }).format(amount)
+}
+
+export function calculatePercentage(
+  current: number,
+  previous: number,
+) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100
+  }
+  return ((current - previous) / previous) * 100
+}
+
+export function fillMissingDays (
+  activeDays: {
+    date: Date
+    income: number
+    expense: number
+  }[],
+  startDate: Date,
+  endDate: Date,
+) {
+  if (activeDays.length === 0) {
+    return []
+  }
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  })
+  const transactionsbyday = allDays.map((day) => {
+      const found = activeDays.find((d) => { return isSameDay(d.date, day) })
+      if (found) {
+          return found
+      } else {
+        return {
+          date: day,
+          income: 0,
+          expense: 0,
+        }
+      }
+  })
+  return transactionsbyday
 }
